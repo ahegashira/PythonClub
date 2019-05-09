9c07d6d2d8a30546da7e3e8cc1c6c1f5ea67d051
Hiro Egashira <ahegashira@gmail.com>
2019-05-09 15:25:37 -0700

    Add files via upload

--- pyclubapp/urls.py
+++ pyclubapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), 
     path('getResources/', views.getResources, name='resources'),
-    path('getMeeting', views.getMeeting, name='meeting'),
+    path('resourceDetails/<int:id>', views.resourceDetails, name='resourcedetails'),
+    path('getMeeting/', views.getMeeting, name='meeting'),
     path('meetingDetails/<int:id>', views.meetingDetails, name='meetingdetails'),
-    path('loginmessage/', views.loginmessage, name='loginmessage'),
-    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
+    # path('loginmessage/', views.loginmessage, name='loginmessage'),
+    # path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 ]
