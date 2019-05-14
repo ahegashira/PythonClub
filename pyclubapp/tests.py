69586eb1e730c49f6300d80b8a60f9a286b180bb
Hiro Egashira <ahegashira@gmail.com>
2019-05-14 11:41:03 -0700

    fix tests

--- pyclubapp/tests.py
+++ pyclubapp/tests.py
@@ -5,10 +5,13 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your tests here.
+
+# ---------- String & Table tests ----------
+
 class MeetingTest(TestCase):
     def test_string(self):
-        meet = Meeting(meeting_location = "BE3179")
-        self.assertEqual(str(meet), meet.meeting_location)
+        mt = Meeting(meeting_title = "Intro to Python")
+        self.assertEqual(str(mt), mt.meeting_title)
 
     def test_table(self):
         self.assertEqual(str(Meeting._meta.db_table), 'meeting')
@@ -19,15 +22,22 @@ class ResourceTest(TestCase):
         self.assertEqual(str(reso), reso.resource_name)
 
     def test_table(self):
-        self.assertEqual(str(Resource._meta.db_table), 'resource_name')
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
 
 class EventTest(TestCase):
     def test_string(self):
-        eventt = Event(event_title = "")
-        self.assertEqual(str(eventt), even.event_title)
+        event = Event(event_title = "testevent")
+        self.assertEqual(str(event), event.event_title)
 
     def test_table(self):
-        self.assertEqual(str(Event._meta.db_table), 'event_title')
+        self.assertEqual(str(Event._meta.db_table), 'event')
+
+class IndexTest(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+# ---------- Basic Views Testing ----------
 
 class IndexTest(TestCase):
     def test_view_url_accessible_by_name(self):
@@ -36,5 +46,14 @@ class IndexTest(TestCase):
 
 class getResourcesTest(TestCase):
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('Resource'))
+        response = self.client.get(reverse('resources'))
         self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
+
+
+
